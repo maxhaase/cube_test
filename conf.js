1f8f124719116f2c972bc550e848f174c81ea4cf
Max Haase <mhaas@kth.se>
2020-08-27 10:58:26 +0200

    Update

--- conf.js
+++ conf.js
@@ -1,11 +1,11 @@
 exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js'],
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js'],
     capabilities: {
         browserName: 'chrome',
         chromeOptions: {
-            args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+            args: ['--headless', '--disable-gpu']
         }
     }
-}
+};
